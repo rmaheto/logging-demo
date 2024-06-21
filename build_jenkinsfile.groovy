de9cb193c957d694e78d8d26bf0cff41c5d1f633
codemaniac <rmkaheto@gmail.com>
2024-06-21 10:37:45 -0500

    refactor

--- build_jenkinsfile.groovy
+++ build_jenkinsfile.groovy
@@ -2,12 +2,15 @@ pipeline {
     agent any
     environment {
         GIT_URL = 'https://github.com/rmaheto/logging-demo.git'
-        BRANCH = 'main'
+        BRANCH = 'main' // Change to your desired branch
+        BUILD_DIR = 'target' // Directory where the built files are located
+        ARTIFACT_NAME = 'your-application.jar' // Change to your WAR or JAR file name
+        PUBLISH_DIR = '/path/to/publish/location' // Local or remote directory
     }
     stages {
         stage('Checkout') {
             steps {
-                git branch: "${BRANCH}", url: "${GIT_URL}"
+                git branch: "${BRANCH}", url: "${GIT_URL}", credentialsId: '61f8848c-29c3-448f-9e37-1f87a4512fd5'
             }
         }
         stage('Build') {
@@ -16,11 +19,22 @@ pipeline {
                 sh 'mvn clean install'
             }
         }
+//        stage('Publish') {
+//            steps {
+//                script {
+//                    // Example for local publishing
+//                    sh "cp ${BUILD_DIR}/${ARTIFACT_NAME} ${PUBLISH_DIR}/"
+//
+//                    // Example for remote publishing via SSH
+//                    // sh "scp ${BUILD_DIR}/${ARTIFACT_NAME} user@remote-server:${PUBLISH_DIR}/"
+//                }
+//            }
+//        }
     }
     post {
         success {
             // Actions to take on successful build
-            echo 'Build completed successfully!'
+            echo 'Build and publish completed successfully!'
         }
         failure {
             // Actions to take on failed build
